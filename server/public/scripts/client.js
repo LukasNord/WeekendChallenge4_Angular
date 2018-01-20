a5a8a1b2d07d058c2561e2d66feea1e3ed306b3d
Lukas Nord <luke.nord@gmail.com>
2018-01-20 10:01:26 -0600

    initial commit with folder and file setup completed

---
+++ server/public/scripts/client.js
@@ -0,0 +1,31 @@
+console.log('js loaded');
+
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('GalleryController', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}]);// end Gallery Controller
