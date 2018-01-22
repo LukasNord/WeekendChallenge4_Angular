e512cb1b69a94cdb3ce516d305285e89e13b778f
Lukas Nord <luke.nord@gmail.com>
2018-01-22 11:38:33 -0600

    this feels hacky but works pretty well for keeping them seperate

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,50 +3,153 @@ console.log('js loaded');
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('GalleryController', [ '$http', function($http){
+myApp.controller('GalleryController1', [ '$http', function($http){
     console.log('GalleryController running');
     const self = this;
 
     self.likeButton = function(){
-        console.log('clicked like button');
-        
+        console.log('clicked like button'); 
     }
-
-
     self.imageObject = {
         show: true,
         textShow:false
     }
-   
-
     self.toggleShowTextOn = function(){
         
         self.imageObject = {
             show: false,
             textShow: true
         }
-
     }
-
     self.toggleShowTextOff = function(){
         self.imageObject = {
             show: true,
             textShow: false
         }
     }
+}]);// end Gallery Controller
+myApp.controller('GalleryController2', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
 
+    self.likeButton = function(){
+        console.log('clicked like button'); 
+    }
+    self.imageObject = {
+        show: true,
+        textShow:false
+    }
+    self.toggleShowTextOn = function(){
+        
+        self.imageObject = {
+            show: false,
+            textShow: true
+        }
+    }
+    self.toggleShowTextOff = function(){
+        self.imageObject = {
+            show: true,
+            textShow: false
+        }
+    }
+}]);// end Gallery Controller
+myApp.controller('GalleryController3', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
 
+    self.likeButton = function(){
+        console.log('clicked like button'); 
+    }
+    self.imageObject = {
+        show: true,
+        textShow:false
+    }
+    self.toggleShowTextOn = function(){
+        
+        self.imageObject = {
+            show: false,
+            textShow: true
+        }
+    }
+    self.toggleShowTextOff = function(){
+        self.imageObject = {
+            show: true,
+            textShow: false
+        }
+    }
+}]);// end Gallery Controller
+myApp.controller('GalleryController4', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
 
+    self.likeButton = function(){
+        console.log('clicked like button'); 
+    }
+    self.imageObject = {
+        show: true,
+        textShow:false
+    }
+    self.toggleShowTextOn = function(){
+        
+        self.imageObject = {
+            show: false,
+            textShow: true
+        }
+    }
+    self.toggleShowTextOff = function(){
+        self.imageObject = {
+            show: true,
+            textShow: false
+        }
+    }
+}]);// end Gallery Controller
+myApp.controller('GalleryController5', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
 
+    self.likeButton = function(){
+        console.log('clicked like button'); 
+    }
+    self.imageObject = {
+        show: true,
+        textShow:false
+    }
+    self.toggleShowTextOn = function(){
+        
+        self.imageObject = {
+            show: false,
+            textShow: true
+        }
+    }
+    self.toggleShowTextOff = function(){
+        self.imageObject = {
+            show: true,
+            textShow: false
+        }
+    }
+}]);// end Gallery Controller
+myApp.controller('GalleryController6', [ '$http', function($http){
+    console.log('GalleryController running');
+    const self = this;
 
-
-
-
-
-
-
-
-
-
-
+    self.likeButton = function(){
+        console.log('clicked like button'); 
+    }
+    self.imageObject = {
+        show: true,
+        textShow:false
+    }
+    self.toggleShowTextOn = function(){
+        
+        self.imageObject = {
+            show: false,
+            textShow: true
+        }
+    }
+    self.toggleShowTextOff = function(){
+        self.imageObject = {
+            show: true,
+            textShow: false
+        }
+    }
 }]);// end Gallery Controller
