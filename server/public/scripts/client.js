d8f11cb4890df7a4d754ab6b2986f9da20770144
Lukas Nord <luke.nord@gmail.com>
2018-01-24 12:28:25 -0600

    removed extra controllers and added ng-repeat

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,27 +6,22 @@ var myApp = angular.module('myApp', []);
 myApp.controller('GalleryController', [ '$http', function($http){
     console.log('GalleryController running');
     const self = this;
-    self.likeCounter = 0;
-    self.likeButton = function(){
-        console.log('clicked like button');
-        self.likeCounter += 1; 
-    }
-    self.imageObject = {
-        show: true,
-        textShow:false,
-        likeCounter: 0
-    }
-    self.toggleShowTextOn = function(){
+    self.pictureList = [
+
         
-        self.imageObject = {
-            show: false,
-            textShow: true
-        }
-    }
-    self.toggleShowTextOff = function(){
-        self.imageObject = {
-            show: true,
-            textShow: false
-        }
-    }
+
+
+
+
+
+    ];
+
+
+
+    
+
+
+
+
+ 
 }]);// end Gallery Controller
